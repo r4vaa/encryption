11c10a87a9166fd996b4833e9871a54b10f4c375
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-04-05 18:51:27 +0530

    using md5

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+const md5 = require('md5')
 
 const app = express();
 
@@ -23,7 +24,7 @@ const userSchema = new mongoose.Schema ({
 // always add this plugin before the user schema, before we create any new users
 
 
-userSchema.plugin(encrypt , { secret : process.env.SECRET ,  encryptedFields: ['password']})
+// userSchema.plugin(encrypt , { secret : process.env.SECRET ,  encryptedFields: ['password']})
 
 
 const User = new mongoose.model('User', userSchema);
@@ -36,6 +37,7 @@ app.get('/login', function(req, res) {
     res.render('login')
 })
 
+
 app.get('/register', function(req, res) {
     res.render('register')
 })
@@ -43,7 +45,7 @@ app.get('/register', function(req, res) {
 app.post('/register', function(req, res) {
     const newUser = new User({
         email : req.body.username,
-        password : req.body.password
+        password : md5(req.body.password)
     })
 
     newUser.save()
@@ -57,7 +59,7 @@ app.post('/register', function(req, res) {
 
 app.post('/login',function(req , res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username})
     .then(function(data){
